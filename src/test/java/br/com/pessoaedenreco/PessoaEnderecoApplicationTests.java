5e79b4a207025ea4d74f82cb409d602b8aa570ab
Ranylson Santos <ranylson1212@gmail.com>
2023-12-12 18:55:19 -0300

    inicia projeto

---
+++ src/test/java/br/com/pessoaedenreco/PessoaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pessoaedenreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
